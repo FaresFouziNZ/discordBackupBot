1103da69575c47b0b9e7874084b6bd0929276234
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2025-07-21 02:11:30 +0300

    Restructure

--- bot/bot.py
+++ bot/bot.py
@@ -13,6 +13,7 @@ KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'discord-topic')
 # Discord client setup
 intents = discord.Intents.default()
 intents.messages = True
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 # Kafka producer setup
@@ -29,15 +30,36 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    if not message.content.startswith('%'):
+        return
+    print(f'Received message: {message.content}')
+    print('new code reloaded')
+    # send_message_to_kafka(message)
 
-    data = {
-        'user': str(message.author),
-        'channel': str(message.channel),
-        'content': message.content,
-        'timestamp': str(message.created_at)
-    }
-
+def send_message_to_kafka(message):
+    data = get_message_data(message)
     producer.send(KAFKA_TOPIC, value=data)
     print(f'Sent to Kafka: {data}')
 
+async def send_channel_history(channel):
+    messages = []
+    async for msg in channel.history(limit=100):
+        messages.append({
+            'user': str(msg.author),
+            'content': msg.content,
+            'timestamp': str(msg.created_at),
+        })
+    return messages
+
+def get_message_data(message):
+    
+    return {
+        'user': str(message.author),
+        'channel': str(message.channel),
+        'content': message.content,
+        'timestamp': str(message.created_at),
+    }
+    
+
 client.run(DISCORD_TOKEN)
+
